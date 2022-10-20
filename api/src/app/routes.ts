3f0a49f7fc0592d121caab55285395ad0a109991
thomasf <fincht96@gmail.com>
2022-10-20 17:03:17 +0100

    feat(api): add ping

--- api/src/app/routes.ts
+++ api/src/app/routes.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
 
+router.get('/ping', (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
 router.post('/email', (req: Request, res: Response) => {
   req.container.resolve('emailController').registerCustomerEmail(req, res);
 });

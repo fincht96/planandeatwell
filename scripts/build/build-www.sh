0e44ebeda7384e4eb25d3c5e9dd15e55cec31877
Chi Nguyen <chinguyen4646@gmail.com>
2022-12-21 13:55:31 +0000

    5. infra stuff and env variables and deps

--- scripts/build/build-www.sh
+++ scripts/build/build-www.sh
@@ -4,6 +4,8 @@
     --build-arg NEXT_PUBLIC_API_URL=https://api.planandeatwell.uk \
     --build-arg NEXT_PUBLIC_GA_TRACKING_ID=G-07YWLRN2XY \
     --build-arg NEXT_PUBLIC_ENV=production \
+    --build-arg NEXT_PUBLIC_CDN=https://newstack.fra1.cdn.digitaloceanspaces.com \
+    --build-arg NEXT_PUBLIC_WWW_BASE_URL=https://planandeatwell.uk \
     -t dokku/planandeatwell_web_prod:latest \
     . 
 

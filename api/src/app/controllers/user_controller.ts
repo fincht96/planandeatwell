0a3102f718ccad1ac340628712b4cb70113ffabd
thomasf <fincht96@gmail.com>
2022-12-23 10:29:11 +0000

    feat(api): add create user account endpoint

--- api/src/app/controllers/user_controller.ts
+++ api/src/app/controllers/user_controller.ts
@@ -7,6 +7,13 @@ const syncClaimsSchema = Joi.object({
   accessToken: Joi.string().required(),
 });
 
+const createAccountSchema = Joi.object({
+  firstName: Joi.string().max(200).required(),
+  lastName: Joi.string().max(200).required(),
+  email: Joi.string().min(0).max(200).required(),
+  password: Joi.string().max(400).required(),
+}).and('firstName', 'lastName', 'email', 'password');
+
 export default class UserController {
   constructor(
     private readonly eventsService: EventsService,
@@ -25,6 +32,31 @@ export default class UserController {
         value.accessToken,
       );
 
+      return res.status(200).json({
+        result,
+        errors: [],
+      });
+    } catch (e: any) {
+      await this.eventsService.insert(
+        'USER_CONTROLLER',
+        'ERROR',
+        e.message ?? '',
+      );
+      return res.status(400).json({
+        errors: [e?.message],
+      });
+    }
+  }
+
+  async createAccount(req: Request, res: Response) {
+    try {
+      const { error, value } = createAccountSchema.validate(req.body);
+
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      const result = await this.userService.createAccount(value);
       return res.status(200).json({
         result,
         errors: [],

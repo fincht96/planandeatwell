81518d32b1486c5e96f96d5cfba67225724bc72b
Chi Nguyen <chinguyen4646@gmail.com>
2023-01-26 12:06:21 +0000

    add

--- api/src/app/firebase_admin.ts
+++ api/src/app/firebase_admin.ts
@@ -2,7 +2,12 @@ import admin from 'firebase-admin';
 import AppConfig from './configs/app.config';
 
 export const initFirebaseAdmin = (appConfig: AppConfig) => {
-  const serviceAccount = JSON.parse(appConfig.firebaseServiceAccount);
+  const stringifiedJSON = Buffer.from(
+    appConfig.firebaseServiceAccount,
+    'base64',
+  ).toString('utf8');
+  const serviceAccount = JSON.parse(stringifiedJSON);
+
   return admin.initializeApp({
     credential: admin.credential.cert(<admin.ServiceAccount>serviceAccount),
   });
